834ee2c244e87f31a895ee931b2648eecb863921
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-16 21:33:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.cpp
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -1,16 +1,12 @@
 class Solution {
   public:
-    // Function to count the number of digits in n that evenly divide n
-    int evenlyDivides(int n) {
-        // code here
-        int num=n;
+    int countDigits(int n) {
         int cnt=0;
         while(n){
-            int val=n%10;
-            if(val!=0 && num%val==0)
-                cnt++;
             n=n/10;
+            cnt++;
         }
         return cnt;
+        
     }
 };
