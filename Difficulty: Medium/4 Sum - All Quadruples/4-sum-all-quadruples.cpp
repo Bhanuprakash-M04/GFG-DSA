87699a1397e0d4fb617005b4660d6d3a80ed6af8
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-18 10:20:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/4 Sum - All Quadruples/4-sum-all-quadruples.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    vector<vector<int>> fourSum(vector<int> &arr, int target) {
+        vector<vector<int>>ans;
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && arr[i]==arr[i-1])
+                continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && arr[j]==arr[j-1])
+                    continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    int sum=arr[i]+arr[j]+arr[k]+arr[l];
+                    if(sum==target){
+                        vector<int>res={arr[i],arr[j],arr[k],arr[l]};
+                        ans.push_back(res);
+                        k++;
+                        l--;
+                        while(k<l && arr[k]==arr[k-1])
+                            k++;
+                        while(k<l && arr[l]==arr[l+1])
+                            l--;
+                    }
+                    else if(sum>target)
+                        l--;
+                    else 
+                        k++;
+                }
+            }
+        }
+        return ans;
+    }
+};
