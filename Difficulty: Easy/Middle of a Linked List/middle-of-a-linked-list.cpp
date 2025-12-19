b1e8c999d156014aa19d83378c2efe6c6b8b713c
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-19 11:32:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}; */
+
+class Solution {
+  public:
+    int getMiddle(Node* head) {
+        if(head->next==NULL)
+            return head->data;
+        Node* fast=head;
+        Node* slow=head;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow->data;
+    }
+};
