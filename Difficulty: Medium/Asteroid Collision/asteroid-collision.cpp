8ccc7ec113bc9a6822fc05aa3262f9ee5be874a8
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-20 22:48:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Asteroid Collision/asteroid-collision.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<int> asteroidCollision(int N, vector<int> &asteroids) {
+        vector<int>ans;
+        stack<int>st;
+        for(int num:asteroids){
+            if(num>0)
+                st.push(num);
+            else{
+                while(!st.empty() && st.top()>0 && st.top()<-num)
+                    st.pop();
+                if(st.empty() || st.top()<0)
+                    st.push(num);
+                if(st.top()==-num)
+                    st.pop();
+            }
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
