551662db7bf59d777f842e2a686300d598249c6d
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2025-12-28 12:46:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -1,15 +1,25 @@
 class Solution {
   public:
     int findMin(int n) {
+        // int ans=0;
+        // ans=ans+(n/10);
+        // n=n%10;
+        // ans=ans+(n/5);
+        // n=n%5;
+        // ans=ans+(n/2);
+        // n=n%2;
+        // ans=ans+(n/1);
+        // n=n%1;
+        // return ans;
+        
+        int arr[4]={1,2,5,10};
         int ans=0;
-        ans=ans+(n/10);
-        n=n%10;
-        ans=ans+(n/5);
-        n=n%5;
-        ans=ans+(n/2);
-        n=n%2;
-        ans=ans+(n/1);
-        n=n%1;
+        for(int i=3;i>=0;i--){
+            while(n>=arr[i]){
+                ans++;
+                n=n-arr[i];
+            }
+        }
         return ans;
     }
 };
