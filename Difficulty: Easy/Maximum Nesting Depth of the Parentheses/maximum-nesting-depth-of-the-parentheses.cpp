09398d19235868cb99f84d503cd506e49902ccbc
Bhanuprakash-M04 <bhanuprakash9501@gmail.com>
2026-01-09 23:24:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Nesting Depth of the Parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxDepth(string s) {
+        int cnt=0;
+        int maxi=0;
+        for(char ch:s){
+            if(ch=='(')
+                cnt++;
+            if(ch==')')
+                cnt--;
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+    }
+};
